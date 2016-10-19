3ef696a1ee62b42f4f631875813f11c9f57829f2
Will Berriel <wberriel@gmail.com>
2016-10-18 22:18:49 -0400

    LutronDB-Parser.py now compiles properly. Updates to device print function

--- LutronDB-Parser.py
+++ LutronDB-Parser.py
@@ -1,42 +1,57 @@
 # Script Name	: LutronDB-Parser.py
 # Author		: William Berriel
 # Created		: March 08  2016
-# Last Modified	: 
+# Last Modified	:
 # Version		: 0.01
 
-# Modifications	: 
+# Modifications	:
 
-# Description	: This module will handle parsing a lutron XML database which is available from a Lutron processor. It should work for RadioRA or HOmeworks QS.
+# Description	: This module will handle parsing a lutron XML database which is
+# available from a Lutron processor. It should work for RadioRA or HOmeworks
+# QS.
 
 
-import xml.etree.ElementTree as ET
+# If possible, import the c implemenation, if not, import normal.
+try:
+        import xmletree.cElementTree as ET
+except ImportError:
+        import xml.etree.ElementTree as ET
+
 
 class LutronDBParser(object):
-    
+
     def __init__(self, DBTree):
         self.DBTree = DBTree
-        
+
         self.Root = self.DBTree.getroot()
-        
-        
+
     @classmethod
     def fromFile(cls, filename):
         return cls(ET.ElementTree(file=filename))
-    
+
     @classmethod
     def fromServer(cls, ipAddress):
         pass
-    
-    def printDevices():
-        for node in DBTree.iter('Area'):
-            areaname = node.attribute.get("Name")
-            for devicegroup in node.iter("DeviceGroups"):
-                devGroupName = devicegroup.attribute.get("Name")
-                for device in devicegroup.iter("Device"):
-                    devName = device.attribute.get("Name")
 
-                    if(devName):
-                         print ('Area %s - DeviceGroup %s - Device Name %s' % areaname, devGroupName, devName)
-    
-    
-test = LutronDBParser.fromFile("eeny demo.xml")
+    def printDevices(self):
+        root = self.DBTree.getroot()
+        areas = root.find("Areas")
+        for node in areas.iter("Area"):
+            areaname = node.attrib.get("Name")
+            print "%s :" % areaname
+
+            # only bottom level Areas can have devices
+            if not len(list(node.find("Areas"))):
+
+                for devicegroup in node.iter("DeviceGroup"):
+                    devGroupName = devicegroup.attrib.get("Name")
+                    print "\t%s group:" % devGroupName
+                    for device in devicegroup.iter("Device"):
+                        devName = device.attrib.get("Name")
+                        devType = device.attrib.get("DeviceType")
+                        integrationID = device.attrib.get("IntegrationID")
+                        if(devName):
+                            print "\t\t%s - %s: ID = %s" % (devName, devType,
+                                                            integrationID)
+test = LutronDBParser.fromFile("eeny_demo.xml")
+test.printDevices()
